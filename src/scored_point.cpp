5d5f28e69eb646c679076137a9a91f3b0fb13815
Unknown <mag@fabrikken>
2009-05-10 12:43:50 +0200

    Implemented scored_point

---
+++ src/scored_point.cpp
@@ -0,0 +1,62 @@
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+#include <ymse/vec.hpp>
+#include "scored_point.hpp"
+#include "skin.hpp"
+
+using ymse::vec2f;
+
+scored_point::scored_point(
+	vec2f pos, float r_,
+	float min_r_,
+	vec2f dir
+) :
+	x(pos[0]), y(pos[1]), r(r_),
+	min_r(min_r_),
+	dx(dir[0]), dy(dir[1])
+{
+}
+
+scored_point::~scored_point() {
+}
+
+void scored_point::head_forward(float) {
+	assert(false);
+	throw std::logic_error("scored_point::head_forward(...) called");
+}
+
+float scored_point::tail_forward(float length) {
+	float l = r*r - min_r*min_r;
+
+	if (length < l) {
+		r = sqrt(r*r - length);
+		return -7.f;
+	} else {
+		return length - l;
+	}
+}
+
+bool scored_point::intersect_with_circle(float x, float y, float r) const {
+
+	// Stub this out. It will be a bit difficult to implement it properly
+	// without having the player crash with it whenever it scores.
+
+	return false;
+}
+
+vec2f scored_point::get_head_pos() const {
+	return vec2f(x, y);
+}
+
+vec2f scored_point::get_tail_pos() const {
+	return vec2f(x, y);
+}
+
+vec2f scored_point::get_head_direction() const {
+	return vec2f(dx, dy);
+}
+
+void scored_point::render(skin& s) const {
+	s.circle(vec2f(x, y), r);
+}
---
+++ src/scored_point.hpp
@@ -0,0 +1,29 @@
+#ifndef SNYGG_SCORED_POINT_HPP
+#define SNYGG_SCORED_POINT_HPP
+
+#include "segment.hpp"
+
+class scored_point : public segment {
+	float x, y, r, min_r, dx, dy;
+
+public:
+	scored_point(
+		ymse::vec2f pos, float r,
+		float min_r,
+		ymse::vec2f dir
+	);
+	~scored_point();
+
+	void head_forward(float);
+	float tail_forward(float);
+
+	bool intersect_with_circle(float x, float y, float r) const;
+
+	ymse::vec2f get_head_pos() const;
+	ymse::vec2f get_tail_pos() const;
+	ymse::vec2f get_head_direction() const;
+
+	void render(skin&) const;
+};
+
+#endif
