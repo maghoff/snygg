a5dbf5826694189aa0ff65cecf6800dab5570111
Unknown <mag@fabrikken>
2009-05-10 11:47:32 +0200

    Factored out the ymse-finder to its own module

---
+++ FindYmse.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 2.6)
+
+find_path(YMSE_PATH ymse/core.hpp $ENV{YMSE_PATH})
+find_library(YMSE_LIBRARY NAMES ymse PATH $ENV{YMSE_PATH}/src)
+
+if (YMSE_PATH AND YMSE_LIBRARY)
+	if (NOT Ymse_FIND_QUIETLY)
+		message(STATUS "Located ymse includes at ${YMSE_PATH} and library at ${YMSE_LIBRARY}")
+	endif (NOT Ymse_FIND_QUIETLY)
+else (YMSE_PATH AND YMSE_LIBRARY)
+	if (Ymse_FIND_REQUIRED)
+		message(FATAL_ERROR
+			"Could not find Ymse. Please set the environmental "
+			"variable YMSE_PATH to the project path. This path "
+			"should contain ymse/core.hpp and src/libymse.a"
+		)
+	endif (Ymse_FIND_REQUIRED)
+endif (YMSE_PATH AND YMSE_LIBRARY)
