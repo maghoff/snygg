ce393fda489b72b1c1f792b03632f6fdeed157f1
Johannes Hoff @ knuth <johshoff@gmail.com>
2009-12-19 14:17:22 +0100

    Added level round and round

---
+++ levels/round_and_round.lua
@@ -0,0 +1,13 @@
+-- Round and round!
+-- Patience might be more valuable than skill...
+
+function create_board()
+    s = segment_sequence()
+    s:push_back(arc(vec(0, 0), 100, 0 * math.pi, 2 * math.pi, 0))
+    s:push_back(arc(vec(0, 0), 80,  1 * math.pi, 2 * math.pi, 0))
+    s:push_back(arc(vec(0, 0), 65,  0 * math.pi, 1 * math.pi, 0))
+    s:push_back(arc(vec(0, 0), 50,  1 * math.pi, 2 * math.pi, 0))
+    s:push_back(arc(vec(0, 0), 35,  0 * math.pi, 1 * math.pi, 0))
+    return s
+end
+
