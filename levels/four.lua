fffe118835680887ac40e3e533981b19fc1adacb
Johannes Hoff @ knuth <johshoff@gmail.com>
2009-12-19 23:29:40 +0100

    New level: four

---
+++ levels/four.lua
@@ -0,0 +1,14 @@
+-- Four
+
+function create_board()
+	shift_x = 10
+    s = segment_sequence()
+    s:push_back(box(rect(-100 + shift_x, -50, 100 + shift_x, 50), 10))
+    for y=-2, 2 do
+        s:push_back(arc(vec(shift_x, y * 20), 3, 2 * math.pi, 0, 1))
+    end
+    for x=-4, 4 do
+        s:push_back(arc(vec(shift_x + x * 20, 0), 3, 2 * math.pi, 0, 1))
+    end
+    return s
+end
