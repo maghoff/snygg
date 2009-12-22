305e97305b25b3f99babb0f263e5bfb3e3ed6ccf
maghoff <unknown>
2009-12-22 23:18:42 +0100

    Updated them_sources.sh to include more files

--- them_sources.sh
+++ them_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find src -name '*.cpp' -or -name '*.hpp' -or -name '*.glsl' | sort > snygg.files
+find src skins levels -name '*.cpp' -or -name '*.hpp' -or -name '*.glsl' -or -name '*.lua' | sort > snygg.files
 
 echo CMakeLists.txt >> snygg.files
 echo src/CMakeLists.txt >> snygg.files
