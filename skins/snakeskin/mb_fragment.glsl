b932b6834708c8c8049277ee141a0bef28ceecf8
maghoff <unknown>
2010-01-05 18:32:33 +0100

    Using a higher exponent for the metaballs-function, making the blood pools more confined. This makes the metaballs effect nice, almost useful, rather than crazy

--- skins/snakeskin/mb_fragment.glsl
+++ skins/snakeskin/mb_fragment.glsl
@@ -3,18 +3,20 @@ uniform sampler1D balls;
 
 varying vec2 world_coord;
 
+float sqr(float x) { return x*x; }
+
 void main(void) {
 	float val = 0;
 	for (int i=0; i<number_of_balls; ++i) {
 		vec3 v = texture(balls, (float(i) + 0.5)/float(number_of_balls));
 		vec2 c = vec2(v.x, v.y) - world_coord;
-		val = val + (10.*v.z*v.z)/(c.x*c.x + c.y*c.y);
+		val = val + sqr(sqr(v.z))/sqr(sqr(c.x) + sqr(c.y));
 	}
 
 	const vec4 color = vec4(1, 0, 0, 1);
 	const vec4 back_color = vec4(0, 0, 0, 1);
 
-	float color_f = smoothstep(10., 11., val);
+	float color_f = smoothstep(1., 1.1, val);
 	float back_color_f = 1 - color_f;
 
 	gl_FragColor = color_f * color + back_color_f * back_color;
