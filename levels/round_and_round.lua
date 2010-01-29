c73e833f07c25dfc793176272958f8e04bb9a28e
maghoff <unknown>
2010-01-29 18:11:23 +0100

    Finishing touches on the sleep-code regarding open_segment

--- levels/round_and_round.lua
+++ levels/round_and_round.lua
@@ -3,11 +3,11 @@
 
 function create_board()
 	s = segment_sequence()
-	s:push_back(arc(vec(0, 0), 100, 0 * math.pi, 2 * math.pi, 0))
-	s:push_back(open_segment(arc(vec(0, 0), 80,  1 * math.pi, 2 * math.pi, 0)))
-	s:push_back(open_segment(arc(vec(0, 0), 65,  0 * math.pi, 1 * math.pi, 0)))
-	s:push_back(open_segment(arc(vec(0, 0), 50,  1 * math.pi, 2 * math.pi, 0)))
-	s:push_back(open_segment(arc(vec(0, 0), 35,  0 * math.pi, 1 * math.pi, 0)))
+	s:push_back(arc(vec(0, 0), 100, 0 * math.pi, 2 * math.pi, 1))
+	s:push_back(open_segment(arc(vec(0, 0), 80,  1 * math.pi, 2 * math.pi, 1)))
+	s:push_back(open_segment(arc(vec(0, 0), 65,  0 * math.pi, 1 * math.pi, 1)))
+	s:push_back(open_segment(arc(vec(0, 0), 50,  1 * math.pi, 2 * math.pi, 1)))
+	s:push_back(open_segment(arc(vec(0, 0), 35,  0 * math.pi, 1 * math.pi, 1)))
 	return s
 end
 
