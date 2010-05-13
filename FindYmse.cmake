cdaac07dd905e10b0d1cdc9038885e53b3e48b7a
maghoff <unknown>
2010-05-13 23:18:21 +0200

    Support build variants in Ymse

--- FindYmse.cmake
+++ FindYmse.cmake
@@ -5,7 +5,16 @@ if (POLICY CMP0011)
 endif (POLICY CMP0011)
 
 find_path(YMSE_PATH ymse/core.hpp $ENV{YMSE_PATH})
-find_library(YMSE_LIBRARY ymse $ENV{YMSE_PATH}/src)
+find_library(YMSE_LIBRARY_DEBUG ymse $ENV{YMSE_PATH}/debug/src)
+find_library(YMSE_LIBRARY_RELEASE ymse $ENV{YMSE_PATH}/release/src)
+
+if ("${CMAKE_BUILD_TYPE}" STREQUAL "debug")
+	set(YMSE_LIBRARY ${YMSE_LIBRARY_DEBUG})
+endif ("${CMAKE_BUILD_TYPE}" STREQUAL "debug")
+
+if (${CMAKE_BUILD_TYPE} STREQUAL "release")
+	set(YMSE_LIBRARY ${YMSE_LIBRARY_RELEASE})
+endif (${CMAKE_BUILD_TYPE} STREQUAL "release")
 
 if (YMSE_PATH AND YMSE_LIBRARY)
 	if (NOT Ymse_FIND_QUIETLY)
@@ -16,7 +25,8 @@ else (YMSE_PATH AND YMSE_LIBRARY)
 		message(FATAL_ERROR
 			"Could not find Ymse. Please set the environmental "
 			"variable YMSE_PATH to the project path. This path "
-			"should contain ymse/core.hpp and src/libymse.a"
+			"should contain ymse/core.hpp, debug/src/libymse.a "
+			"and release/src/libymse.a"
 		)
 	endif (Ymse_FIND_REQUIRED)
 endif (YMSE_PATH AND YMSE_LIBRARY)
--- GCCSetup.cmake
+++ GCCSetup.cmake
@@ -1,6 +1,4 @@
-
-# Unable to use -D_GLIBCXX_DEBUG due to other libraries that collide with this (ymse, perhaps?)
+# -D_GLIBCXX_DEBUG makes it crash :(
 SET(CMAKE_CXX_FLAGS_DEBUG "-ggdb -DEBUG -DDEBUG -ffast-math -pipe -Wall -Werror" CACHE STRING "LOL" FORCE)
-
 SET(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -ffast-math -pipe -Wall -Werror -Wl,-s" CACHE STRING "LOL" FORCE)
 
