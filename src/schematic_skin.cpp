c9335a4cea0354bfc0a3f9d5f899d19efd92292b
maghoff <unknown>
2011-04-17 18:31:28 +0200

    Draw arrows in the segments in schematic_skin

--- src/schematic_skin.cpp
+++ src/schematic_skin.cpp
@@ -6,6 +6,11 @@
 #include "complex_polygon.hpp"
 #include "schematic_skin.hpp"
 
+namespace {
+	const float ARROW_LENGTH = 4.;
+	const float ARROW_WIDTH = 1.2;
+}
+
 schematic_skin::schematic_skin() { }
 schematic_skin::~schematic_skin() { }
 
@@ -39,9 +44,11 @@ void schematic_skin::blood(ymse::vec2f p, float r) {
 	plain_skin::circle(p, r);
 }
 
-void schematic_skin::fat_arc(ymse::vec2f p, float r, float t, float begin, float end, float b_begin, float b_end) {
+void schematic_skin::fat_arc(ymse::vec2f p, float r, float t, float begin_in, float end_in, float b_begin, float b_end) {
 	float r1 = r-t, r2 = r+t;
 	float step_size = get_step_size(r2);
+
+	float begin = begin_in, end = end_in;
 	if (begin > end) std::swap(begin, end);
 
 	float &x(p[0]), &y(p[1]);
@@ -73,8 +80,38 @@ void schematic_skin::fat_arc(ymse::vec2f p, float r, float t, float begin, float
 	glVertex2f(x + r2 * cos(end), y + r2 * sin(end));
 	glEnd();
 
+	if ((end - begin) * r > ARROW_LENGTH * 1.25) {
+		const float dir = begin_in < end_in ? 1. : -1.;
+		const float hrl = dir * (ARROW_LENGTH / 2. / r);
+		const float center = (begin + end) / 2.;
+		const float arrow_begin = center - hrl;
+		const float arrow_end = center + hrl;
+		const float arrowhead_end = center + hrl - dir * ARROW_WIDTH / r;
+
+		glBegin(GL_LINE_STRIP);
+		float abegin = arrow_begin, aend = arrow_end;
+		if (abegin > aend) std::swap(abegin, aend);
+		for (float d = abegin; d < aend; d += step_size) {
+			glVertex2f(x + r * cos(d), y + r * sin(d));
+		}
+		glVertex2f(x + r * cos(aend), y + r * sin(aend));
+		glEnd();
+
+		glBegin(GL_LINES);
+		glVertex2f(x + r * cos(arrow_end), y + r * sin(arrow_end));
+		glVertex2f(x + (r - ARROW_WIDTH) * cos(arrowhead_end), y + (r - ARROW_WIDTH) * sin(arrowhead_end));
+
+		glVertex2f(x + r * cos(arrow_end), y + r * sin(arrow_end));
+		glVertex2f(x + (r + ARROW_WIDTH) * cos(arrowhead_end), y + (r + ARROW_WIDTH) * sin(arrowhead_end));
+		glEnd();
+	}
+
 	glColor4f(0.7, 1.0, 0.7, 0.3);
 	plain_skin::fat_arc(p, r, t, begin, end, b_begin, b_end);
+}
+
+inline void glyVertex(const ymse::vec2f& v) {
+	glVertex2f(v.x(), v.y());
 }
 
 void schematic_skin::fat_line(ymse::vec2f p, ymse::vec2f d, float len, float t, float b_begin, float b_end) {
@@ -92,6 +129,24 @@ void schematic_skin::fat_line(ymse::vec2f p, ymse::vec2f d, float len, float t, 
 	glVertex2f(x2 - nx, y2 - ny);
 	glVertex2f(x1 - nx, y1 - ny);
 	glEnd();
+
+	if (len > ARROW_LENGTH * 1.25) {
+		const float hll = ARROW_LENGTH / 2.;
+		const ymse::vec2f center = p + d * (len / 2.f);
+		const ymse::vec2f long_normal = ymse::vec2f(nx, ny);
+		const ymse::vec2f normal = long_normal * (1.f / long_normal.length());
+
+		glBegin(GL_LINES);
+		glyVertex(center - hll * d);
+		glyVertex(center + hll * d);
+
+		glyVertex(center + hll * d);
+		glyVertex(center + (hll - ARROW_WIDTH) * d + ARROW_WIDTH * normal);
+
+		glyVertex(center + hll * d);
+		glyVertex(center + (hll - ARROW_WIDTH) * d - ARROW_WIDTH * normal);
+		glEnd();
+	}
 
 	glColor4f(0.7, 0.7, 1.0, 0.3);
 	plain_skin::fat_line(p, d, len, t, b_begin, b_end);
