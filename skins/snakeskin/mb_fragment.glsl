dd1ede6b97e79278722e7b60b6e201cb23e690bd
maghoff <unknown>
2011-04-21 11:32:00 +0200

    Fix for jagged edges in metaballs.

--- skins/snakeskin/mb_fragment.glsl
+++ skins/snakeskin/mb_fragment.glsl
@@ -14,7 +14,7 @@ void main(void) {
 	}
 
 	const vec4 color = vec4(0.7, 0, 0, 1);
-	const vec4 back_color = vec4(0, 0, 0, 0);
+	const vec4 back_color = vec4(0.7, 0, 0, 0);
 
 	float color_f = smoothstep(1., 1.1, val);
 	float back_color_f = 1. - color_f;
