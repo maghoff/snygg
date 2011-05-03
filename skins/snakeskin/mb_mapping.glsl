111c2098e0c68037335b026bc69d1250941690b0
maghoff <unknown>
2011-05-03 22:30:29 +0200

    The blood now has depth. The height-function might need tuning

--- skins/snakeskin/mb_mapping.glsl
+++ skins/snakeskin/mb_mapping.glsl
@@ -5,23 +5,35 @@ uniform vec4 ambient;
 
 vec4 directional_light(vec3 normal, vec3 light, vec4 diffuse, float phong_exponent, float local_variance);
 
+float h(vec2 p) {
+	float val = texture2D(storedValue, p).r;
+	return 1. - 1./val;
+}
+
 void main(void) {
 	const vec3 light_pos = vec3(0, 0, 3);
-	const vec3 normal = vec3(0, 0, 1);
 	const vec4 diffuse = vec4(0.7, 0, 0, 1);
-	const float phong_exponent = 7.0;
+	const float phong_exponent = 200.0;
 	const float local_variance = 1.0;
 
 	float val = texture2D(storedValue, screen_coord).r;
 	float alpha = smoothstep(1., 1.1, val);
 
-	vec3 w = vec3(world_coord, 0);
+	vec3 w = vec3(world_coord, h(screen_coord));
 	vec3 light = normalize(light_pos - w);
+
+	const float a = 3.0;
+	const float s = 1.0/800.0;
+	vec3 t = vec3(1, 0, a*(h(screen_coord + s*vec2(1, 0)) - h(screen_coord + s*vec2(-1, 0))));
+	vec3 b = vec3(0, 1, a*(h(screen_coord + s*vec2(0, 1)) - h(screen_coord + s*vec2(0, -1))));
+	vec3 normal = normalize(cross(t, b));
 
 	vec4 color =
 		diffuse * ambient +
 		directional_light(normal, light, diffuse, phong_exponent, local_variance)
 	;
+
+	//color = vec4(normal, 1);
 
 	gl_FragColor = vec4(color.rgb, alpha);
 }
