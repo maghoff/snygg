d6840d4782db7b71b63e77237dcc1d689c7d1398
Magnus Hoff <maghoff@gmail.com>
2014-02-17 17:17:17 +0100

    Include "own" header file on top to discover missing #includes

--- src/fileutil/fileutil.cpp
+++ src/fileutil/fileutil.cpp
@@ -1,8 +1,8 @@
+#include "fileutil.hpp"
 #include <stdexcept>
 #include <fstream>
 #include <string>
 #include <vector>
-//#include "fileutil.hpp"
 
 void read_entire_file(std::vector<char>& target, const std::string& filename) {
 	std::ifstream in(filename.c_str(), std::ios::binary);
