44e0f9690c086d3114d511530f1b1e58a33fae21
Magnus Hoff <maghoff@gmail.com>
2014-03-02 17:51:57 +0100

    Simplified by taking build variant out of the equation in serving the pnacl module

--- nacl/index.js
+++ nacl/index.js
@@ -9,16 +9,8 @@ var directoryResourceFactory = function (fullpath) { return new resource_tree.Di
 
 server = resource_tree.createServer(new resource_tree.SerialLookup([
 	{
-		"pnacl": {
-			"Debug": {
-				"platform-pnacl.nmf": new resource_tree.FileResource(path.join(root, "nacl/src/manifest.json"), {'content-type': 'application/octet-stream'}),
-				"platform-pnacl.pexe": new resource_tree.FileResource(path.join(root, "build-pnacl/debug/src/platform-pnacl/platform-pnacl.pexe"), {'content-type': 'application/octet-stream'}),
-			},
-			"Release": {
-				"platform-pnacl.nmf": new resource_tree.FileResource(path.join(root, "nacl/src/manifest.json"), {'content-type': 'application/octet-stream'}),
-				"platform-pnacl.pexe": new resource_tree.FileResource(path.join(root, "build-pnacl/release/src/platform-pnacl/platform-pnacl.pexe"), {'content-type': 'application/octet-stream'}),
-			},
-		},
+		"platform-pnacl.nmf": new resource_tree.FileResource(path.join(root, "nacl/src/manifest.json"), {'content-type': 'application/octet-stream'}),
+		"platform-pnacl.pexe": new resource_tree.FileResource(path.join(root, "build-pnacl/debug/src/platform-pnacl/platform-pnacl.pexe"), {'content-type': 'application/octet-stream'}),
 		"levels": new resource_tree.FileLookup(path.join(root, "levels"))
 	},
 	new resource_tree.FileLookup(path.join(root, "nacl", "src")),
