42885285a9fafeaab1f0c1ea3e4ed5b07881a84a
Magnus Hoff <maghoff@gmail.com>
2014-04-03 12:49:03 +0200

    Better error reporting for urlbuf errors

--- src/platform-pnacl/nacl_errors.hpp
+++ src/platform-pnacl/nacl_errors.hpp
@@ -8,7 +8,9 @@
 enum class err : int {
 	graphics3d_context_lost = 10,
 	unable_to_create_3d_context,
-	unable_to_bind_3d_context
+	unable_to_bind_3d_context,
+	urlloader_open,
+	urlloader_readresponsebody
 };
 
 void fail(err, const std::string&);
--- src/platform-pnacl/urlbuf.cpp
+++ src/platform-pnacl/urlbuf.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <ppapi/cpp/url_request_info.h>
 #include <ppapi/cpp/completion_callback.h>
+#include "nacl_errors.hpp"
 
 urlbuf::urlbuf(pp::InstanceHandle instanceHandle, const std::string& url) :
 	buffer(4096)
@@ -13,14 +14,14 @@ urlbuf::urlbuf(pp::InstanceHandle instanceHandle, const std::string& url) :
 
 	auto result = urlLoader.Open(urlRequestInfo, pp::BlockUntilComplete());
 
-	if (result != PP_OK) throw std::runtime_error("urlLoader.Open returned an error code");
+	if (result != PP_OK) fail(err::urlloader_open, "urlLoader.Open returned an error code");
 }
 
 urlbuf::~urlbuf() { }
 
 urlbuf::int_type urlbuf::underflow() {
 	auto bytesRead = urlLoader.ReadResponseBody(buffer.data(), buffer.size(), pp::BlockUntilComplete());
-	if (bytesRead < 0) throw std::runtime_error("urlLoader.ReadResponseBody returned an error code");
+	if (bytesRead < 0) fail(err::urlloader_readresponsebody, "urlLoader.ReadResponseBody returned an error code");
 
 	setg(buffer.data(), buffer.data(), buffer.data() + bytesRead);
 
