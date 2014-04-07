a81724fa8b1ea2d1956f07b07f1a8b08ee7df08a
Magnus Hoff <maghoff@gmail.com>
2014-04-07 10:17:26 +0200

    Only show each user's best score in highscorelist for a given board

--- nacl/src/highscore-list.js
+++ nacl/src/highscore-list.js
@@ -24,8 +24,6 @@ define([
 			startkey: JSON.stringify([board]),
 			endkey: JSON.stringify([board, {}]),
 			inclusive_end: false,
-			reduce: true,
-			group_level: 3,
 			limit: 10
 		};
 
@@ -34,9 +32,8 @@ define([
 			queryArgs += "&" + encodeURIComponent(key) + "=" + encodeURIComponent(opts[key])
 		}
 		queryArgs = queryArgs.substr(1);
-
 		ajax({
-			url: this.centralCouch + "snygg/_design/snygg/_view/highscores?" + queryArgs
+			url: this.centralCouch + "personal_records/_design/snygg/_view/highscore_lists?" + queryArgs
 		}, function (err, data) {
 			if (this.desiredBoard !== board) return;
 			this.clear();
@@ -45,24 +42,30 @@ define([
 			var tbody = this.dom.getElementsByTagName("tbody")[0];
 			var prevScore = null;
 			for (var i = 0; i < data.rows.length; ++i) {
+				var value = {
+					name: data.rows[i].value[0],
+					score: data.rows[i].value[1],
+					timestamp: data.rows[i].value[2]
+				};
+
 				var row = document.createElement("tr");
 
 				var rank = document.createElement("td");
 				rank.classList.add("rank-col");
-				if (data.rows[i].value.score !== prevScore) {
+				if (value.score !== prevScore) {
 					rank.textContent = (i+1) + ".";
-					prevScore = data.rows[i].value.score;
+					prevScore = value.score;
 				}
 				row.appendChild(rank);
 
 				var player = document.createElement("td");
 				player.classList.add("player-col");
-				player.textContent = data.rows[i].value.name;
+				player.textContent = value.name;
 				var gravatar = (function () {
 					var gravatar = document.createElement("img");
 					gravatar.classList.add("highscore-gravatar");
 					gravatar.setAttribute("src", "http://www.gravatar.com/avatar/?d=mm&s=50");
-					this.gravatarProvider.get(data.rows[i].value.name, function (gravatarUrl) {
+					this.gravatarProvider.get(value.name, function (gravatarUrl) {
 						gravatar.setAttribute("src", gravatarUrl + "&s=50");
 					});
 					return gravatar;
@@ -72,7 +75,7 @@ define([
 
 				var score = document.createElement("td");
 				score.classList.add("score-col");
-				score.textContent = data.rows[i].value.score;
+				score.textContent = value.score;
 				row.appendChild(score);
 
 				tbody.appendChild(row);
